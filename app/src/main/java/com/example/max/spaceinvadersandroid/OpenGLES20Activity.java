12000c35b8becd32b6f32aa05840403d3b1d1b2a
Maxi Lencina <lencinamaxiezequiel@gmail.com>
2018-01-24 19:10:20 -0300

    drawing shapes

---
+++ app/src/main/java/com/example/max/spaceinvadersandroid/OpenGLES20Activity.java
@@ -0,0 +1,27 @@
+package com.example.max.spaceinvadersandroid;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class OpenGLES20Activity extends Activity {
+
+    private GLSurfaceView mGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+}
